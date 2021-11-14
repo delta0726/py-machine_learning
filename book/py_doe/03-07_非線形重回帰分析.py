2e258a440c14b93bdd8c35e7f4571c35b86f8532
Keido Sumi <delta0726@nta.pial.jp>
2021-11-15 07:53:57 +0900

    Add py_doe chapter-3

--- book/py_doe/03-07_非線形重回帰分析.py
+++ book/py_doe/03-07_非線形重回帰分析.py
@@ -1,15 +1,18 @@
 # ******************************************************************************
 # Title     : Pythonで学ぶ実験計画法
 # Chapter   : 3 データ解析や回帰分析の手法
-# Theme     : 回帰モデルの推定精度の評価（外部バリデーション）
-# Date      : 2021/11/10
-# Page      : P38 - P49
+# Theme     : 非線形重回帰分析
+# Date      : 2021/11/15
+# Page      : P49 - P51
 # ******************************************************************************
 
 
 # ＜概要＞
-# - モデルの推定精度の評価方法として外部バリデーションを確認する
-#   --- 外部バリデーションは訓練データでモデル構築、テストデータで精度評価を行う
+# - XとYの非線形関係を表現する最も単純な方法は、特徴量に｢二条項｣｢交差項｣を追加すること
+#   --- 特徴量が多いと交差項は非常に多くなってしまう点に注意（パターンを絞ったほうが現実的）
+#   --- ｢二乗｣が適切かどうかは不明（0.5乗/指数関数/対数関数などが適切な可能性もある）
+# - 非線形性を回帰分析で表現するには、事前にXとYの関係性に対する事前知識を持ってモデリングする必要がある
+#   --- 機械学習アルゴリズムは事前知識が少なくてもモデリングが可能
 
 
 # ＜目次＞
@@ -33,7 +36,6 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
 
-
 # データ準備
 df = pd.read_csv('csv/resin.csv', index_col=0, header=0)
 
@@ -48,47 +50,56 @@ df = pd.read_csv('csv/resin.csv', index_col=0, header=0)
 # --- 目的変数
 # --- 説明変数
 y = df.iloc[:, 0]
-x = df.iloc[:, 1:]
+original_x = df.iloc[:, 1:]
+
+# 説明変数の二条項や交差項を追加
+x = original_x.copy()
+x_square = original_x ** 2
+
+for i in range(original_x.shape[1]):
+    for j in range(original_x.shape[1]):
+        if i == j:  # 二乗項
+            x = pd.concat(
+                [x, x_square.rename(columns={x_square.columns[i]: '{0}^2'.format(x_square.columns[i])}).iloc[:, i]],
+                axis=1)
+        elif i < j:  # 交差項
+            x = pd.concat([x, original_x.iloc[:, i] * original_x.iloc[:, j]], axis=1)
+            x = x.rename(columns={0: '{0}*{1}'.format(x_square.columns[i], x_square.columns[j])})
 
 
-# 2 データ分割 -------------------------------------------------------------
+# 2 データ分割 ---------------------------------------------------------------
 
 # テストデータのサンプル数
-number_of_test_samples = 5
+number_of_test_sample = 5
 
 # データ分割
-if number_of_test_samples == 0:
+if number_of_test_sample == 0:
     x_train = x.copy()
     x_test = x.copy()
     y_train = y.copy()
     y_test = y.copy()
 else:
-    x_train, x_test, y_train, y_test = \
-        train_test_split(x, y, test_size=number_of_test_samples,
-                         shuffle=True, random_state=99)
+    x_train, x_test, y_train, y_test = train_test_split(x, y,
+                                                        test_size=number_of_test_sample,
+                                                        shuffle=True, random_state=99)
 
 
-# 3 データの標準化 --------------------------------------------------------
-
-# ＜ポイント＞
-# - 特徴量を全て標準化すると定数項がゼロとなる
-
+# 3 データの標準化 -----------------------------------------------------------
 
 # ゼロ・バリアンス・フィルタ
-deleting_variables = x.columns[x_train.std() == 0]
+deleting_variables = x_train.columns[x_train.std() == 0]
 x_train = x_train.drop(deleting_variables, axis=1)
 x_test = x_test.drop(deleting_variables, axis=1)
 
-# データの標準化
+# オートスケーリング
 autoscaled_y_train = (y_train - y_train.mean()) / y_train.std()
 autoscaled_x_train = (x_train - x_train.mean()) / x_train.std()
 
 
-# 4 モデル構築 --------------------------------------------------------------
+# 4 モデル構築 ------------------------------------------------------------------
 
 # ＜ポイント＞
-# - 線形回帰モデルに標準化したデータを適用すると標準線形回帰モデルとなる
-#   --- 目的変数と説明変数の平均が全てゼロとなることで定数項もゼロとなる（重要）
+# - 特徴量を全て標準化すると定数項がゼロとなる
 
 
 # インスタンス生成
@@ -110,19 +121,15 @@ standard_regression_coefficients = \
 
 # 5 モデルによる予測 -----------------------------------------------------------
 
-# ＜ポイント＞
-# - 訓練データで作成したモデルで訓練データを予測（インサンプル）
-
-
 # トレーニングデータの推定
 autoscaled_estimated_y_train = model.predict(autoscaled_x_train)
 
 # スケールを元に戻す
-estimated_y_train = autoscaled_y_train * y_train.std() + y_train.mean()
+estimated_y_train = autoscaled_estimated_y_train * y_train.std() + y_train.mean()
 
 # データフレーム格納
-estimated_y_train = pd.DataFrame(estimated_y_train, index=x_train.index)\
-        .set_axis(['estimated_y'], axis=1)
+estimated_y_train = pd.DataFrame(estimated_y_train, index=x_train.index,
+                                 columns=['estimated_y'])
 
 
 # 6 プロット作成 --------------------------------------------------------------
@@ -168,12 +175,6 @@ mean_absolute_error(y_true=y_train, y_pred=estimated_y_train)
 
 # 8 テストデータによる検証 --------------------------------------------------
 
-# ＜ポイント＞
-# - 訓練データで構築したモデルをテストデータで検証する
-#   --- 外部バリデーションの場合のモデル精度の評価
-#   --- テストデータは全体の25％である5レコードしか存在しない
-#   --- サンプル数の絶対数が少ないと信頼性が低くなる点が問題
-
 # データ標準化
 # --- 訓練データに対して基準化する点に注意
 autoscaled_x_test = (x_test - x_train.mean()) / x_train.std()
@@ -189,7 +190,7 @@ estimated_y_test = pd.DataFrame(estimated_y_test,index=x_test.index)\
         .set_axis(['estimated_y'], axis=1)
 
 # メトリック出力
-# --- R2は0.886
+# --- R2は0.991
 r2_score(y_true=y_test, y_pred=estimated_y_test)
 mean_squared_error(y_true=y_test, y_pred=estimated_y_test, squared=False)
 mean_absolute_error(y_true=y_test, y_pred=estimated_y_test)
