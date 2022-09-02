d8b4a15e71a7e32b18a637459742cd273325a0e1
Keido Sumi <delta0726@nta.pial.jp>
2022-09-03 07:34:40 +0900

    Add chap2-1

--- book/MathProgramming/chap2-1_行動パターンの類似度.py
+++ book/MathProgramming/chap2-1_行動パターンの類似度.py
@@ -3,7 +3,7 @@
 # Chapter     : 2 機械学習を使った分析を行ってみよう
 # Theme       : 2-1 顧客の行動パターンの類似度を計算しよう
 # Creat Date  : 2021/12/18
-# Final Update:
+# Final Update: 2022/9/2
 # Page        : P60 - P63
 # ******************************************************************************
 
@@ -28,6 +28,7 @@ import matplotlib.pyplot as plt
 
 
 # データ読み込み
+# --- 日付列をインデックスにする
 df_info = pd.read_csv("csv/accomodation_info.csv", index_col=0, parse_dates=[0])
 
 # データ確認
@@ -37,13 +38,16 @@ df_info
 # 1 特徴ベクトルの可視化 ------------------------------------------------------------
 
 # ＜ポイント＞
-# - 月ごとの顧客の利用回数を特徴量とする
-# - 特徴量を顧客ごとに定義する（行：日時 列：顧客ID）
+# - 月ごとの顧客の利用回数を特徴量として顧客ごとの類似度を確認する
+#   --- 時系列の折れ線グラフを重ね合わせて類似度を確認する
 
 
 # indexの抽出
+# --- 月末日付のインデックス
+# --- インデックスのみのDataFrameを作成
 x_0 = df_info.resample('M').count()
 x_0 = x_0.drop(x_0.columns.values, axis=1)
+type(x_0)
 
 # 順位の設定
 i_rank = 1
@@ -53,7 +57,8 @@ j_rank = 2
 i_id = df_info['顧客ID'].value_counts().index[i_rank]
 j_id = df_info['顧客ID'].value_counts().index[j_rank]
 
-# 月ごとの利用回数を特徴量として抽出
+# 月次カウントの取得
+# --- 月ごとの利用回数を特徴量として抽出
 x_i = df_info.loc[lambda x: x['顧客ID'] == i_id].resample('M').count()
 x_j = df_info.loc[lambda x: x['顧客ID'] == j_id].resample('M').count()
 
@@ -62,7 +67,8 @@ x_j = df_info.loc[lambda x: x['顧客ID'] == j_id].resample('M').count()
 x_i = pd.concat([x_0, x_i], axis=1).fillna(0)
 x_j = pd.concat([x_0, x_j], axis=1).fillna(0)
 
-# 描画
+# プロット作成
+# --- 系列の連動性から類似度を確認する
 plt.plot(x_i)
 plt.plot(x_j)
 plt.xticks(rotation=60)
@@ -77,10 +83,11 @@ plt.show()
 
 
 # 特徴ベクトルの差を計算
+# --- ランク1とランク2の距離
 dx = x_i.iloc[:, 0].values - x_j.iloc[:, 0].values
 
 # 距離の計算
-# --- ベクトルノルム
+# --- ユークリッド距離の算出
 n = np.linalg.norm(dx)
 
 # 次元による正規化
